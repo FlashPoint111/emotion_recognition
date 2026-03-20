ed1fa8851390ffdefe10a59b04c5ac0d5270a335
Mike <luhaoyu_123@126.com>
2026-03-19 22:39:37 -0400

    20260320

--- dataset/preprocess_dataset_DFEW.py
+++ dataset/preprocess_dataset_DFEW.py
@@ -1,32 +1,48 @@
 import os
+import random
+
+from pyinstrument import Profiler
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import yaml
 
 
-def run_preprocessing(config):
-    dataset_dir = config['dataset_dir']
-    dataset_info_dir = config['label_dir']
-    emotions = ["anger", "disgust", "fear", "happiness", "sadness", "surprise", "contempt", "anxiety", "helplessness", "disappointment", "neutral"]
+def run_preprocessing(config, mode):
+    dataset_dir = config["dataset"]["dir"]
+    dataset_frames_dir = config["dataset"]['frames_dir']
+    if mode == 'train':
+        dataset_info_dir = config["train"]['label_dir']
+    else:
+        dataset_info_dir = config["test"]['label_dir']
+
     preprocessed_data = []
 
-    with open(dataset_info_dir, 'r') as f:
-        info = f.readlines()
+    info = pd.read_csv(dataset_info_dir)
+    with tqdm(total=len(info)) as pbar:
+        for video_info in info.itertuples():
+            video_file, label = video_info[1], video_info[2]
+            video_file = str(video_file)
+            video_path = os.path.join(dataset_dir, video_file + '.flac')
+            frame_path = os.path.join(dataset_frames_dir, str.zfill(video_file, 5))
+            frame_num = len(os.listdir(frame_path))
+            if frame_num < 3:
+                continue
+            label_idx = label
 
-    for video_info in info:
-        video_file, label = video_info.strip().split(' ')
-        video_path = os.path.join(dataset_dir, video_file)
-        label_idx = emotions.index(label)
-
-        video_info = {
-            "video_path": video_path,
-            "label": label_idx
-        }
-        preprocessed_data.append(video_info)
+            video_info = {
+                "video_path": video_path,
+                "frame_path": frame_path,
+                "frame_num": frame_num,
+                "label": label_idx,
+            }
+            preprocessed_data.append(video_info)
+            pbar.update(1)
 
     return preprocessed_data
 
 
 if __name__ == '__main__':
-    config = {
-        "dataset_dir": "F:/MAFW/data/clips",
-        "label_dir": "F:/MAFW/Train & Test Set/single/no_caption/set_1/train.txt",
-    }
-    preprocessed_data = run_preprocessing(config)
+    with open("../config/AIM2.yaml", "r") as f:
+        config = yaml.safe_load(f)
+    preprocessed_data = run_preprocessing(config, mode="train")
